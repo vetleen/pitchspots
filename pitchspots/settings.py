5f473554176ebe3ea0be60b68002eb725fe34958
Vetle Engesbak <vetleengesbak@gmail.com>
2013-05-21 21:10:01 +0200

    fixed bug in settings.py

--- pitchspots/settings.py
+++ pitchspots/settings.py
@@ -143,7 +143,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    #'todotracker',
+    'generic_parts',
 )
 
 # A sample logging configuration. The only tangible logging

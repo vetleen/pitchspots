5a923a7d67069ef50beb3952891a8a75c078b61b
Vetle Engesbak <vetleengesbak@gmail.com>
2013-08-09 18:28:02 +0200

    trying out tests

--- ps_editor/logic.py
+++ ps_editor/logic.py
@@ -1,6 +1,15 @@
 #from django.contrib.auth.models import User
 #from django.contrib.auth import authenticate, login, logout
+from ps_editor.models import Pitchspot
+
+def create_pitchspot(title, owner, administrator, is_published, date_completed):
+    Pitchspot.objects.create(title=title, owner=request.user, administrator=request.user, date_created=datetime.utcnow().replace(tzinfo=utc), is_published=False)    
 
 
-def edit_pitchspot(): 
-    return "Hello World!"
+
+
+#def edit_pitchspot(title, owner, administrator, is_published, date_completed):
+
+
+
+    

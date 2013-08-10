68be26c352c50d915dcd10092574ecb38110861a
Vetle Engesbak <vetleengesbak@gmail.com>
2013-08-10 08:19:42 +0200

    testing

--- ps_editor/views.py
+++ ps_editor/views.py
@@ -24,7 +24,8 @@ def frontpageview(request):
 def create_pitchspot_view(request):
     title = "Test"#str(request.POST['title'])
     is_published = bool(request.POST['is_published'])
+    print "is_published is %s, and its type is: %s" % (is_published, type(is_published)) 
     owner = request.user
-    logic.create_pitchspot(title=title, owner=owner, is_published=is_published)
+    logic.create_pitchspot(title=title, owner=owner)
     output = "It works.."
     return HttpResponse(output)

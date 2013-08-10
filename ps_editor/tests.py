1493278c818308b251ea55a2475f67b2e15ff743
Vetle Engesbak <vetleengesbak@gmail.com>
2013-08-10 08:58:45 +0200

    testing

--- ps_editor/tests.py
+++ ps_editor/tests.py
@@ -1,13 +1,7 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+from datetime import datetime  
 
 from django.test import TestCase
 from django.test.client import Client
-
 from django.contrib.auth.models import User
 
 import ps_editor.logic
